7b09cf0c780cc5e63ddb8cb29cbb120c460142b3
Ranajit Chandra <40000140+Ron22c@users.noreply.github.com>
2025-06-22 13:04:06 +0530

    Delete AUDIOX directory

--- AUDIOX/services/text-extractor/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-REDIS_HOST = os.getenv("REDIS_HOST", "redis")
-REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-PDF_UPLOAD_CHANNEL = "pdf_uploaded"
-
-TEMP_DIR = "/tmp/pdfs"
--- AUDIOX/services/tts-generator/app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-REDIS_HOST = os.getenv("REDIS_HOST", "redis")
-REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-TAGGED_INPUT_CHANNEL = "tagged_text"
-
-AUDIO_OUTPUT_DIR = "/tmp/audio"
-BUCKET_NAME = "audiobooks"
